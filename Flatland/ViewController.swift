78d142d61a0e5fecf472be56050a41af9001594a
Stuart Rankin <sjrankin@gmail.com>
2020-03-26 14:52:30 +0900

    Initial Commit

---
+++ Flatland/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 3/26/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
