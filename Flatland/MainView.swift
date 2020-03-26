76a9b9849d57f5e8b765df607af0a339fdaf6aa6
Stuart Rankin <sjrankin@gmail.com>
2020-03-26 22:31:10 +0900

    Update MainView.swift
    Continued initial implementation.

--- Flatland/MainView.swift
+++ Flatland/MainView.swift
@@ -10,36 +10,145 @@ import Foundation
 import UIKit
 import CoreImage
 
-class MainView: UIViewController, CAAnimationDelegate
+class MainView: UIViewController, CAAnimationDelegate, SettingsProtocol
 {
+    /// Original orientation of the image with Greenwich, England as the baseline. Since this program
+    /// treats midnight as the base and the image has Greenwich at the top, we need an offset value
+    /// to make sure the image is rotated correctly.
     let OriginalOrientation: Double = 180.0
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        Settings.Initialize()
+        WorldViewer.image = UIImage(named: PreviousImage)
         InitializeGrid()
+        UpdateSunLocations()
         TopView.backgroundColor = UIColor.black
         StartTimeLabel()
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        OriginalTimeFrame = MainTimeLabel.frame
+        OriginalSunFrame = SunView.frame
+    }
+    
+    var OriginalTimeFrame: CGRect = CGRect.zero
+    var OriginalSunFrame: CGRect = CGRect.zero
+    
+    func SettingsDone()
+    {
+        for Layer in GridOverlay.layer.sublayers!
+        {
+            if Layer.name == "ViewGrid"
+            {
+                Layer.removeFromSuperlayer()
+            }
+        }
+        if Settings.ShowGrid()
+        {
+            InitializeGrid()
+        }
+        UpdateSunLocations()
+        var ProvisionalImage = ""
+        switch Settings.GetImageCenter()
+        {
+            case .NorthPole:
+                ProvisionalImage = "WorldNorth"
+            
+            case .SouthPole:
+                ProvisionalImage = "WorldSouth"
+        }
+        if ProvisionalImage != PreviousImage
+        {
+            PreviousImage = ProvisionalImage
+            WorldViewer.image = UIImage(named: PreviousImage)
+        }
+        if Settings.GetTimeLabel() == .None
+        {
+            MainTimeLabel.isHidden = true
+        }
+        else
+        {
+            MainTimeLabel.isHidden = false
+        }
+        PreviousPercent = -1.0
+    }
+    
+    func UpdateSunLocations()
+    {
+        switch Settings.GetSunLocation()
+        {
+            case .Hidden:
+                SunView.isHidden = true
+            
+            case .Top:
+                SunView.isHidden = false
+                if Settings.GetTimeLabel() != .None
+                {
+                    MainTimeLabel.frame = OriginalTimeFrame
+                }
+                SunView.frame = OriginalTimeFrame
+            
+            case .Bottom:
+                SunView.isHidden = false
+                if Settings.GetTimeLabel() != .None
+                {
+                    MainTimeLabel.frame = CGRect(x: OriginalTimeFrame.origin.x,
+                                                 y: OriginalSunFrame.origin.y,
+                                                 width: OriginalTimeFrame.size.width,
+                                                 height: OriginalTimeFrame.size.height)
+                }
+                SunView.frame = CGRect(x: SunView.frame.origin.x,
+                                       y: OriginalTimeFrame.origin.y,
+                                       width: SunView.frame.size.width,
+                                       height: SunView.frame.size.height)
+        }
     }
     
     func InitializeGrid()
     {
         GridOverlay.backgroundColor = UIColor.clear
         let Grid = CAShapeLayer()
-        Grid.bounds = GridOverlay.frame//GridOverlay.bounds
+        Grid.name = "ViewGrid"
+        Grid.bounds = GridOverlay.frame
         Grid.frame = GridOverlay.frame
         let Lines = UIBezierPath()
         let CenterH = Grid.bounds.size.width / 2.0
         let CenterV = Grid.bounds.size.height / 2.0
-        Lines.move(to: CGPoint(x: CenterH, y: 0))
-        Lines.addLine(to: CGPoint(x: CenterH, y: Grid.frame.size.height))
-        Lines.move(to: CGPoint(x: 0, y: CenterV))
-        Lines.addLine(to: CGPoint(x: Grid.frame.size.width, y: CenterV))
-        let Equator = UIBezierPath(ovalIn: CGRect(x: CenterH / 2,
-                                                  y: CenterV / 2,
-                                                  width: CenterH,
-        height: CenterV))
-        Lines.append(Equator)
+        if Settings.ShowNoonMeridians()
+        {
+            Lines.move(to: CGPoint(x: CenterH, y: 0))
+            Lines.addLine(to: CGPoint(x: CenterH, y: Grid.frame.size.height))
+            Lines.move(to: CGPoint(x: 0, y: CenterV))
+            Lines.addLine(to: CGPoint(x: Grid.frame.size.width, y: CenterV))
+        }
+        if Settings.ShowEquator()
+        {
+            let Equator = UIBezierPath(ovalIn: CGRect(x: CenterH / 2,
+                                                      y: CenterV / 2,
+                                                      width: CenterH,
+                                                      height: CenterV))
+            Lines.append(Equator)
+        }
+        if Settings.ShowTropics()
+        {
+            let TropicDistance: CGFloat = 23.43666
+            let TropicPercent = Grid.bounds.size.width * (TropicDistance / 180.0)
+            let CancerWidth = CenterH - TropicPercent
+            let Cancer = UIBezierPath(ovalIn: CGRect(x: CenterH - (CancerWidth / 2.0),
+                                                     y: CenterV - (CancerWidth / 2.0),
+                                                     width: CancerWidth,
+                                                     height: CancerWidth))
+            let CapricornWidth = CenterH + TropicPercent
+            let Capricorn = UIBezierPath(ovalIn: CGRect(x: CenterH - (CapricornWidth / 2.0),
+                                                        y: CenterV - (CapricornWidth / 2.0),
+                                                        width: CapricornWidth,
+                                                        height: CapricornWidth))
+            Lines.append(Cancer)
+            Lines.append(Capricorn)
+        }
         Grid.strokeColor = UIColor.systemYellow.withAlphaComponent(0.5).cgColor
         Grid.lineWidth = 1.0
         Grid.fillColor = UIColor.clear.cgColor
@@ -92,30 +201,25 @@ class MainView: UIViewController, CAAnimationDelegate
         {
             return
         }
-        print("Rotating image to \(Percent)%")
         PreviousPercent = Percent
-        let Degrees = 360.0 * Percent - OriginalOrientation
+        let FinalOffset = Settings.GetSunLocation() == .Bottom ? 0.0 : OriginalOrientation
+        let Degrees = 360.0 * Percent - FinalOffset
         let Radians = Degrees * Double.pi / 180.0
-        #if true
         let Rotation = CATransform3DMakeRotation(CGFloat(-Radians), 0.0, 0.0, 1.0)
         WorldViewer.layer.transform = Rotation
-        #else
-//        WorldViewer.transform = WorldViewer.transform.rotated(by: CGFloat(Radians))
-//        WorldViewer.transform = CGAffineTransform(rotationAngle: CGFloat(Radians))
-        
-        let Rotation = CABasicAnimation(keyPath: "transform.rotation.z")
-        Rotation.delegate = self
-        //Rotation.fromValue = NSNumber(floatLiteral: 0.0)
-        Rotation.toValue = NSNumber(floatLiteral: -Radians)
-        Rotation.duration = 0.75
-        Rotation.repeatCount = 0
-        Rotation.isAdditive = true
-        WorldViewer.layer.add(Rotation, forKey: "RotateMe")
-        #endif
     }
     
+    var PreviousImage: String = "WorldNorth"
     var PreviousPercent: Double = -1.0
-
+    
+    @IBSegueAction func InstantiateSettingsNavigator(_ coder: NSCoder) -> SettingsNavigationViewer?
+    {
+        let Controller = SettingsNavigationViewer(coder: coder)
+        Controller?.Delegate = self
+        return Controller
+    }
+    
+    @IBOutlet weak var SettingsButton: UIButton!
     @IBOutlet weak var GridOverlay: UIView!
     @IBOutlet weak var SunView: UIImageView!
     @IBOutlet weak var MainTimeLabel: UILabel!
