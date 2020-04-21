5a216e64498e0c8e9e67ca9f7bbcfd79c43d1c8f
Stuart Rankin <sjrankin@gmail.com>
2020-04-21 21:10:37 +0900

    Create DebugNavigationViewer.swift
    Code to run the debug navigation controller.

---
+++ Flatland/Settings/DebugNavigationViewer.swift
@@ -0,0 +1,15 @@
+//
+//  DebugNavigationViewer.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 4/21/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DebugNavigationViewer: UINavigationController
+{
+    weak var Delegate: SettingsProtocol? = nil
+}
