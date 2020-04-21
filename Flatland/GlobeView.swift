2800f4f4c8067c19d874704f4c5a11f5e347369b
Stuart Rankin <sjrankin@gmail.com>
2020-04-21 15:56:35 +0900

    Update GlobeView.swift
    Added code to correctly draw minor grid lines.

--- Flatland/GlobeView.swift
+++ Flatland/GlobeView.swift
@@ -391,12 +391,64 @@ class GlobeView: SCNView
         }
     }
     
-    func MakeGridLines(Width: CGFloat, Height: CGFloat, LineColor: UIColor) -> UIImage
+    /// Draw an image with grid lines on it that can be used as a texture on top of a global image.
+    /// - Note: This function draws the minor grid lines. It calls another function to draw the
+    ///         major grid lines on top of the minor grid lines.
+    /// - Parameter Width: Width of the image to return.
+    /// - Parameter Height: Height of the image to return.
+    /// - Parameter LineColor: The major grid line color.
+    /// - Parameter MinorLineColor: The minor grid line color.
+    /// - Returns: Image of the specified size with the grid lines drawn as per user settings.
+    func MakeGridLines(Width: CGFloat, Height: CGFloat,
+                       LineColor: UIColor = UIColor.red,
+                       MinorLineColor: UIColor = UIColor.yellow) -> UIImage
     {
         let Base = MakeImageBase(Width: Width, Height: Height, FillColor: UIColor.clear)
         UIGraphicsBeginImageContext(Base.size)
         
         Base.draw(at: .zero)
+        
+        let ctx = UIGraphicsGetCurrentContext()
+        ctx?.setLineWidth(3.0)
+        ctx?.setStrokeColor(MinorLineColor.withAlphaComponent(0.5).cgColor)
+        
+        if Settings.ShowMinorGridLines()
+        {
+        let Gap = Settings.GetMinorGridLineGap()
+        if Gap >= 5.0
+        {
+            for Longitude in stride(from: 0.0, to: 359.0, by: Gap)
+            {
+                let X = Width * CGFloat(Longitude / 360.0)
+                ctx?.move(to: CGPoint(x: X, y: 0))
+                ctx?.addLine(to: CGPoint(x: X, y: Height))
+                ctx?.strokePath()
+            }
+            for Latitude in stride(from: 0.0, to: 359.0, by: Gap)
+            {
+                let Y = Height * CGFloat(Latitude / 360.0)
+                ctx?.move(to: CGPoint(x: 0, y: Y))
+                ctx?.addLine(to: CGPoint(x: Width, y: Y))
+                ctx?.strokePath()
+            }
+        }
+        }
+        
+        var Final = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        //Draw the major grid lines on top of the minor grid lines.
+            let FinalWithMajorLines = DrawMajorGridLines(On: Final!, Width: Width, Height: Height,
+                                                         LineColor: LineColor)
+                Final = FinalWithMajorLines
+        return Final!
+    }
+    
+    func DrawMajorGridLines(On Image: UIImage, Width: CGFloat, Height: CGFloat,
+                            LineColor: UIColor = UIColor.yellow) -> UIImage
+    {
+        UIGraphicsBeginImageContext(Image.size)
+        
+        Image.draw(at: .zero)
         
         let ctx = UIGraphicsGetCurrentContext()
         ctx?.setLineWidth(5.0)
@@ -415,13 +467,12 @@ class GlobeView: SCNView
             let X = Width * CGFloat(Latitude.rawValue)
             ctx?.move(to: CGPoint(x: X, y: 0))
             ctx?.addLine(to: CGPoint(x: X, y: Height))
-            
-            
             ctx?.strokePath()
         }
         
         let Final = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return Final!
+        
     }
 }
