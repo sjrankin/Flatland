bbea270e6306113504922f8dd2208ce158a48367
Stuart Rankin <sjrankin@gmail.com>
2020-04-28 21:52:59 +0900

    Update GlobeView.swift
    Initial implementation of relative time. Changed moonlight to use a .directional light and cast shadows. Added speical shape for home location.

--- Flatland/GlobeView.swift
+++ Flatland/GlobeView.swift
@@ -74,12 +74,17 @@ class GlobeView: SCNView
         LightNode.position = SCNVector3(0.0, 0.0, 80.0)
         
         let MoonLight = SCNLight()
-        MoonLight.type = .omni
-        MoonLight.intensity = 200
-        MoonLight.color = UIColor.gray
+        MoonLight.type = .directional
+        MoonLight.intensity = 300
+        MoonLight.castsShadow = true
+        MoonLight.shadowColor = UIColor.black.withAlphaComponent(0.80)
+        MoonLight.shadowMode = .forward
+        MoonLight.shadowRadius = 10.0
+        MoonLight.color = UIColor.cyan
         MoonNode = SCNNode()
         MoonNode.light = MoonLight
         MoonNode.position = SCNVector3(0.0, 0.0, -100.0)
+        MoonNode.eulerAngles = SCNVector3(180.0 * CGFloat.pi / 180.0, 0.0, 0.0)
         
         self.scene?.rootNode.addChildNode(CameraNode)
         self.scene?.rootNode.addChildNode(LightNode)
@@ -174,17 +179,28 @@ class GlobeView: SCNView
     func UpdateEarth(With Percent: Double)
     {
         let Degrees = 180.0 - (360.0) * Percent
-        //print("Percent(\(Percent))=\(Degrees.RoundedTo(4))°")
         let Radians = Degrees.Radians
         let Rotate = SCNAction.rotateTo(x: 0.0, y: CGFloat(-Radians), z: 0.0, duration: 1.0)
         EarthNode?.runAction(Rotate)
         SeaNode?.runAction(Rotate)
         LineNode?.runAction(Rotate)
+        #if false
+        if Settings.GetHourValueType() == .RelativeToLocation
+        {
+            let HourRotation = SCNAction.rotateTo(x: 0.0, y: 0.0, z: CGFloat(-Radians), duration: 1.0)
+            HourNode?.runAction(HourRotation)
+        }
+        else
+        {
+            let Reset = SCNAction.rotateTo(x: 0.0, y: 0.0, z: 0.0, duration: 0.1)
+            HourNode?.runAction(Reset)
+        }
+        #endif
     }
     
     var IgnoreClock = false
     
-    /// Draws a cubical Earth for not other reason than being silly.
+    /// Draws a cubical Earth for no other reason than being silly.
     func ShowCubicEarth()
     {
         EarthNode?.removeAllActions()
@@ -361,7 +377,7 @@ class GlobeView: SCNView
         
         let Declination = Sun.Declination(For: Date())
         SystemNode?.eulerAngles = SCNVector3(Declination.Radians, 0.0, 0.0)
-        HourNode?.eulerAngles = SCNVector3(Declination.Radians, 0.0, 0.0)
+        //HourNode?.eulerAngles = SCNVector3(Declination.Radians, 0.0, 0.0)
         
         PlotCities(On: EarthNode!, WithRadius: 10)
         
@@ -444,16 +460,31 @@ class GlobeView: SCNView
                 break
                 
                 case .RelativeToLocation:
-                    if let LocalLon = Settings.GetLocalLongitude()
+                    if let LocalLongitude = Settings.GetLocalLongitude()
                     {
-                        let LonHour = LocalLon / 15.0
-                        let ILonHour = Int(LonHour)
-                        DisplayHour = ILonHour - DisplayHour
+                        let Long = Int(LocalLongitude / 15.0)
+                        print("Hour longitude: \(Long), Local longitude: \(LocalLongitude)")
+                        DisplayHour = Hour - 12 - 1
+                        if DisplayHour < -12
+                        {
+                            DisplayHour = 12 - (DisplayHour * -1) % 12
+                        }
+                        DisplayHour = DisplayHour * -1
+                        DisplayHour = DisplayHour - Long
+                        if DisplayHour >= 12
+                        {
+                            DisplayHour = 12 - (DisplayHour % 12)
+                            DisplayHour = DisplayHour * -1
+                        }
+                        if DisplayHour < -12
+                        {
+                            DisplayHour = (12 + (DisplayHour % 12))
+                        }
                         if DisplayHour > 0
                         {
                             Prefix = "+"
                         }
-                    }
+                }
                 
                 case .RelativeToNoon:
                     DisplayHour = DisplayHour - 12
@@ -486,6 +517,45 @@ class GlobeView: SCNView
         SeaNode?.opacity = CGFloat(Alpha)
     }
     
+    /// Add the home location to the globe.
+    /// - Note: The home location has shifting colors and rotates to make it obviouse where it is.
+    /// - Parameter Latitude: The latitude of the home location.
+    /// - Parameter Longitude: The longitude of the home location.
+    /// - Parameter ToSurface: The parent node.
+    func AddLocation(Latitude: Double, Longitude: Double, ToSurface: SCNNode)
+    {
+        let (X, Y, Z) = ToECEF(Latitude, Longitude, Radius: 10)
+        let Home = SCNNode(geometry: SCNBox(width: 5, height: 5, length: 5, chamferRadius: 0.5))
+        Home.name = "HomeLocation"
+        Home.scale = SCNVector3(0.04, 0.04, 0.04)
+        Home.geometry?.firstMaterial?.diffuse.contents = UIColor(hue: 0.0, saturation: 1.0, brightness: 0.5, alpha: 1.0)
+        Home.castsShadow = true
+        Home.position = SCNVector3(X, Y, Z)
+        ToSurface.addChildNode(Home)
+        let ChangeDuration: CGFloat = 3.0
+        var HueValue = 0.0
+        let ColorAction = SCNAction.customAction(duration: Double(ChangeDuration))
+        {
+            (Node, ElapsedTime) in
+            HueValue = HueValue + 0.01
+            if HueValue > 1.0
+            {
+                HueValue = 0.0
+            }
+            Node.geometry?.firstMaterial?.diffuse.contents = UIColor(hue: CGFloat(HueValue), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+            Node.geometry?.firstMaterial?.emission.contents = UIColor(hue: CGFloat(HueValue), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        }
+        let ColorForever = SCNAction.repeatForever(ColorAction)
+        Home.runAction(ColorForever)
+        let RotateValue = CGFloat.pi / 180.0 * 360.0
+        let Rotator = SCNAction.rotateBy(x: RotateValue * 1.1,
+                                         y: RotateValue * 0.9,
+                                         z: RotateValue * 0.5,
+                                         duration: Double(ChangeDuration * 3.7))
+        let RotateForever = SCNAction.repeatForever(Rotator)
+        Home.runAction(RotateForever)
+    }
+    
     /// Plot cities on the Earth.
     /// - Parameter On: The main sphere node upon which to plot cities.
     /// - Parameter WithRadius: The radius of there Earth sphere node.
@@ -501,6 +571,14 @@ class GlobeView: SCNView
             UserCity.CityColor = Color
             UserCity.IsUserCity = true
             TestCities.append(UserCity)
+        }
+        
+        if let LocalLongitude = Settings.GetLocalLongitude()
+        {
+            if let LocalLatitude = Settings.GetLocalLatitude()
+            {
+                AddLocation(Latitude: LocalLatitude, Longitude: LocalLongitude, ToSurface: On)
+            }
         }
         
         let CitySize: CGFloat = 0.15
