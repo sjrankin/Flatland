9453b30961d9479604e23f8f4cef5aec7410fdd1
Stuart Rankin <sjrankin@gmail.com>
2020-05-07 20:44:48 +0900

    Update Extensions.swift
    Added extension to UIImage to apply an alpha value to an image.

--- Flatland/Utilities/Extensions.swift
+++ Flatland/Utilities/Extensions.swift
@@ -518,6 +518,16 @@ extension UIImage
     {
         return Rotate(Radians: Degrees.Radians)
     }
+    
+    //https://stackoverflow.com/questions/28517866/how-to-set-the-alpha-of-an-uiimage-in-swift-programmatically
+    func Alpha(_ Value: CGFloat) -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: CGPoint.zero, blendMode: .normal, alpha: Value)
+        let NewImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return NewImage!
+    }
 }
 
 // MARK: - UIColor extensions.
