de84279569c97d41bb02de0f80c63fe49c7ccdaf
Stuart Rankin <sjrankin@gmail.com>
2020-05-13 16:05:04 +0900

    Create GeneralSettingsController.swift
    Code for controlling the general settings navigation controller.

---
+++ Flatland/Menus/GeneralSettingsController.swift
@@ -0,0 +1,18 @@
+//
+//  GeneralSettingsController.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 5/12/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GeneralSettingsController: UINavigationController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
