02a81e1a3c8bf26d1be0fa297301ffab18e14825
Stuart Rankin <sjrankin@gmail.com>
2020-05-14 20:27:12 +0900

    Create +MenuHandler.swift
    Added code to initialize the new "context" menu.

---
+++ Flatland/MainViewExtensions/+MenuHandler.swift
@@ -0,0 +1,37 @@
+//
+//  +MenuHandler.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 5/14/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension MainView: UIPopoverPresentationControllerDelegate, MenuControllerDelegate
+{
+    func ShowSettingsWindow()
+    {
+        let Storyboard = UIStoryboard(name: "Menus", bundle: nil)
+        if let Controller = Storyboard.instantiateViewController(identifier: "GeneralSettingsMenuRoot") as? GeneralSettingsController
+        {
+            Controller.Delegate = self
+            Controller.modalPresentationStyle = .popover
+            if let PresentingController = Controller.presentationController
+            {
+                PresentingController.delegate = self
+            }
+            self.present(Controller, animated: true, completion: nil)
+            if let PopView = Controller.popoverPresentationController
+            {
+                PopView.sourceView = SettingsButton
+                PopView.sourceRect = SettingsButton.bounds
+            }
+        }
+    }
+    
+    func SettingsClosed()
+    {
+    }
+}
