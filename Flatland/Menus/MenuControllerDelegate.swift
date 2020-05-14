9b2682bf4a3a8ffbf0f00ebb7f775bb91e839542
Stuart Rankin <sjrankin@gmail.com>
2020-05-14 20:27:31 +0900

    Create MenuControllerDelegate.swift
    Placeholder for setting change communication protocol.

---
+++ Flatland/Menus/MenuControllerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  MenuControllerDelegate.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 5/14/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol MenuControllerDelegate: class
+{
+    func SettingsClosed()
+}
