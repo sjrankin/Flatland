3b3dc8ebbba9cea044cb9df3601c2550c4b8d90f
Stuart Rankin <sjrankin@gmail.com>
2020-05-16 15:59:12 +0900

    Create MainProtocol.swift
    Protocol to allow objects to reference certain functions/objects in the main window class.

---
+++ Flatland/MainProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MainProtocol.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 5/16/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol MainProtocol: class
+{
+    func GlobeObject() -> GlobeView?
+}
