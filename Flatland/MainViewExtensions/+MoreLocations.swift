3f7a896bf469899e414d717e5d8b3967664da920
Stuart Rankin <sjrankin@gmail.com>
2020-05-18 22:52:02 +0900

    Update +MoreLocations.swift
    Code to load world heritage data.

--- Flatland/MainViewExtensions/+MoreLocations.swift
+++ Flatland/MainViewExtensions/+MoreLocations.swift
@@ -11,18 +11,61 @@ import UIKit
 
 extension MainView
 {
-    func LoadWorldHeritageSites()
+    func LoadWorldHeritageSites() -> WorldHeritageSites?
     {
         if let FileURL = Bundle.main.url(forResource: "WorldHeritageSites2019", withExtension: "json")
         {
             do
             {
-                
+                let RawData = try Data(contentsOf: FileURL)
+                let Decoder = JSONDecoder()
+                let HeritageSites = try Decoder.decode(WorldHeritageSites.self, from: RawData)
+                return HeritageSites
             }
             catch
             {
-                print("Error loading world heritage sites: \(error)")
+                fatalError("Error loading world heritage sites: \(error.localizedDescription)")
             }
         }
+        print("Error finding WorldHeritageSites2019.json in bundle.")
+        return nil
+    }
+}
+
+struct WorldHeritageSites: Decodable
+{
+    var Sites: [WorldHeritageSite]
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case Sites = "Sites"
+    }
+}
+
+struct WorldHeritageSite: Decodable
+{
+    var UID: Int
+    var ID: Int
+    var Name: String
+    var DateInscribed: Int
+    var Longitude: Double
+    var Latitude: Double
+    var Hectares: Double
+    var Category: String
+    var CategoryShort: String
+    var Countries: String
+    
+    enum CodingKeys: String, CodingKey
+    {
+        case UID = "UID"
+        case ID = "ID"
+        case Name = "Name"
+        case DateInscribed = "DateInscribed"
+        case Longitude = "Longitude"
+        case Latitude = "Latitude"
+        case Hectares = "Hectares"
+        case Category = "Category"
+        case CategoryShort = "CategoryShort"
+        case Countries = "Countries"
     }
 }
