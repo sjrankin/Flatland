c2875c5051ff43e0eb001a26c05aaeab1de0ab4e
Stuart Rankin <sjrankin@gmail.com>
2020-05-19 15:38:12 +0900

    Create WorldHeritageSite.swift
    Class that holds one world heritage site.

---
+++ Flatland/MainViewExtensions/WorldHeritageSite.swift
@@ -0,0 +1,40 @@
+//
+//  WorldHeritageSite.swift
+//  Flatland
+//
+//  Created by Stuart Rankin on 5/19/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WorldHeritageSite
+{
+    init(_ UID: Int, _ ID: Int, _ Name: String, _ Year: Int, _ Latitude: Double,
+         _ Longitude: Double, _ Hectares: Double, _ Category: String,
+         _ ShortCategory: String, _ Countries: String)
+    {
+        self.UID = UID
+        self.ID = ID
+        self.Name = Name
+        self.DateInscribed = Year
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.Hectares = Hectares
+        self.Category = Category
+        self.ShortCategory = ShortCategory
+        self.Countries = Countries
+    }
+    
+    var UID: Int = 0
+    var ID: Int = 0
+    var Name: String = ""
+    var DateInscribed: Int = 0
+    var Longitude: Double = 0.0
+    var Latitude: Double = 0.0
+    var Hectares: Double? = nil
+    var Category: String = ""
+    var ShortCategory: String = ""
+    var Countries: String = ""
+}
